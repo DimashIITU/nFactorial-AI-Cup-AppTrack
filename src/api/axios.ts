9c5e25c1c01c1e816f5e86da3b9579a9dbae85e8
DimashIITU <dinmuhamedserik4@gmail.com>
2023-04-09 14:01:25 +0600

    lastCom

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
-export const chatToken = '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';
+export const chatToken =
+  process.env.AI_KEY || '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';
 
 export const instance = axios.create({
   headers: {
